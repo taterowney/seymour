3ecd42a3cb7973f23340f512a17dd6a0ce77b565
madvorak <martin.dvorak@matfyz.cz>
2024-12-31 14:35:36 +0100

    elaboration time optimization

--- Seymour/ForMathlib/MatrixTU.lean
+++ Seymour/ForMathlib/MatrixTU.lean
@@ -46,37 +46,25 @@ lemma in_set_range_singType_cast_iff_abs [LinearOrderedCommRing R] (a : R) :
     a ∈ Set.range SignType.cast ↔ |a| ∈ Set.range SignType.cast := by
   sorry
 
-attribute [-simp] Fintype.card_ofIsEmpty Fintype.card_ofSubsingleton -- major performance issue
-
 lemma Matrix.fromBlocks_isTotallyUnimodular [LinearOrderedCommRing R]
     [Fintype X₁] [DecidableEq X₁] [Fintype X₂] [DecidableEq X₂] [Fintype Y₁] [DecidableEq Y₁] [Fintype Y₂] [DecidableEq Y₂]
     {A₁ : Matrix X₁ Y₁ R} {A₂ : Matrix X₂ Y₂ R} (hA₁ : A₁.IsTotallyUnimodular) (hA₂ : A₂.IsTotallyUnimodular) :
     (fromBlocks A₁ 0 0 A₂).IsTotallyUnimodular := by
   -- different proof strategy attempted: https://github.com/madvorak/matrix-tu-experimental/blob/main/MatrixTuExperimental.lean
   intro k f g hf hg
-  rw [isTotallyUnimodular_iff] at hA₁ hA₂
-  rw [fromBlocks_submatrix]
+  rw [Matrix.isTotallyUnimodular_iff_fintype] at hA₁ hA₂
+  rw [Matrix.fromBlocks_submatrix]
   -- look at sizes of submatrices in blocks
   if hxy : Fintype.card { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd }
          = Fintype.card { y₁ : Fin k × Y₁ // g y₁.fst = Sum.inl y₁.snd }
          ∧ Fintype.card { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd }
          = Fintype.card { y₂ : Fin k × Y₂ // g y₂.fst = Sum.inr y₂.snd }
   then -- square case
-    obtain ⟨cardi₁, cardi₂⟩ := hxy
-    -- equivalences between canonical indexing types of given cardinality and current indexing types
-    -- (domains of parts of indexing functions)
-    let eX₁ :
-      Fin (Fintype.card { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd }) ≃ { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd } :=
-        (Fintype.equivFin { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd }).symm
-    let eX₂ :
-      Fin (Fintype.card { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd }) ≃ { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd } :=
-        (Fintype.equivFin { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd }).symm
-    let eY₁ :
-      Fin (Fintype.card { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd }) ≃ { y₁ : Fin k × Y₁ // g y₁.fst = Sum.inl y₁.snd } :=
-        (cardi₁ ▸ Fintype.equivFin { y₁ : Fin k × Y₁ // g y₁.fst = Sum.inl y₁.snd }).symm
-    let eY₂ :
-      Fin (Fintype.card { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd }) ≃ { y₂ : Fin k × Y₂ // g y₂.fst = Sum.inr y₂.snd } :=
-        (cardi₂ ▸ Fintype.equivFin { y₂ : Fin k × Y₂ // g y₂.fst = Sum.inr y₂.snd }).symm
+    -- bijections between indexing types of equal cardinalities
+    let eY₁ : { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd } ≃ { y₁ : Fin k × Y₁ // g y₁.fst = Sum.inl y₁.snd } :=
+      Fintype.equivOfCardEq hxy.1
+    let eY₂ : { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd } ≃ { y₂ : Fin k × Y₂ // g y₂.fst = Sum.inr y₂.snd } :=
+      Fintype.equivOfCardEq hxy.2
     -- relating submatrices in blocks to submatrices of `A₁` and `A₂`
     have hAfg :
       (fromBlocks
@@ -92,15 +80,13 @@ lemma Matrix.fromBlocks_isTotallyUnimodular [LinearOrderedCommRing R]
           g.decomposeSum
       =
       (fromBlocks
-        (A₁.submatrix
-          (((·.val.snd) : { x₁ : Fin k × X₁ // f x₁.fst = Sum.inl x₁.snd } → X₁) ∘ eX₁)
+        (A₁.submatrix (·.val.snd)
           (((·.val.snd) : { y₁ : Fin k × Y₁ // g y₁.fst = Sum.inl y₁.snd } → Y₁) ∘ eY₁)
         ) 0 0
-        (A₂.submatrix
-          (((·.val.snd) : { x₂ : Fin k × X₂ // f x₂.fst = Sum.inr x₂.snd } → X₂) ∘ eX₂)
+        (A₂.submatrix (·.val.snd)
           (((·.val.snd) : { y₂ : Fin k × Y₂ // g y₂.fst = Sum.inr y₂.snd } → Y₂) ∘ eY₂)
         )).submatrix
-          (f.decomposeSum.trans (Equiv.sumCongr eX₁.symm eX₂.symm))
+          f.decomposeSum
           (g.decomposeSum.trans (Equiv.sumCongr eY₁.symm eY₂.symm))
     · ext
       simp only [Function.decomposeSum, Equiv.coe_fn_mk, Equiv.coe_trans, Equiv.sumCongr_apply, Function.comp_apply,
@@ -114,7 +100,7 @@ lemma Matrix.fromBlocks_isTotallyUnimodular [LinearOrderedCommRing R]
         · simp
     -- absolute value of determinant was preserved by previous mappings,
     -- and we now express it as a product of determinants of submatrices in blocks
-    rw [hAfg, in_set_range_singType_cast_iff_abs, abs_det_submatrix_equiv_equiv, Matrix.det_fromBlocks_zero₁₂,
+    rw [hAfg, in_set_range_singType_cast_iff_abs, Matrix.abs_det_submatrix_equiv_equiv, Matrix.det_fromBlocks_zero₁₂,
       ←in_set_range_singType_cast_iff_abs]
     -- determinants of submatrices in blocks are `0` or `±1` by TUness of `A₁` and `A₂`
     apply in_set_range_singType_cast_mul_in_set_range_singType_cast
