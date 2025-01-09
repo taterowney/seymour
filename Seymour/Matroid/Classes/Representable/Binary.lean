0fb287b12bb6587271dd81f6c6af767716cf889a
madvorak <martin.dvorak@matfyz.cz>
2025-01-09 09:08:12 +0100

    BinaryMatroid.dual_isBinary

--- Seymour/Matroid/Classes/Representable/Binary.lean
+++ Seymour/Matroid/Classes/Representable/Binary.lean
@@ -10,14 +10,17 @@ section Definitions
 /-- Binary matroid is vector matroid of matrix over `Z2`. -/
 abbrev BinaryMatroid (α : Type) := VectorMatroid α Z2
 
-/-- Matroid `M` is binary if it is representable over Z2 -/
+/-- Matroid `M` is binary if it is representable over `Z2` -/
 def Matroid.IsBinary (M : Matroid α) : Prop :=
   M.IsRepresentableOver Z2
 
-/-- Every `BinaryMatroid` is representable over Z2 -/
-lemma BinaryMatroid.IsBinary (M : BinaryMatroid α) : M.toMatroid.IsBinary := by use M
+/-- Every `BinaryMatroid` is representable over `Z2` -/
+lemma BinaryMatroid.isBinary (M : BinaryMatroid α) : M.toMatroid.IsBinary := by
+  use M
 
 /-- The dual of a binary matroid is binary -/
-lemma BinaryMatroid.dual_IsBinary (M : BinaryMatroid α) : M.toMatroid.dual.IsBinary := sorry
+lemma BinaryMatroid.dual_isBinary [DecidableEq α] (M : BinaryMatroid α) : M.toMatroid✶.IsBinary :=
+  have ⟨S, hS⟩ := M.dual_exists_standardRepr
+  ⟨S.toVectorMatroid, hS.symm⟩
 
 end Definitions
